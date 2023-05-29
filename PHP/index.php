669edb91cb5b011e6e254bc90ef45d46a985215d
Ana Ramirez <aramirez.cac@gmail.com>
2023-05-29 09:21:11 -0400

    clase 27, herencia en php

--- PHP/index.php
+++ PHP/index.php
@@ -1,4 +1,13 @@
 <?php
+require_once('car.php');
+require_once('uberX.php');
+require_once('uberPool.php');
+require_once('account.php');
 
+$uberX = new UberX("CVB123",new Account("Andres","AND456","TOYOTA","VITS"));
+$uberX->printDataCar();
+
+$uberPool = new UberPool("CVB123",new Account("Andrea","ANDA123","Chevrolet","Spark"))
+$uberPool->printDataCar();
 
 ?>
